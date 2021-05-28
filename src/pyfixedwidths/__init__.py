1734c80528a707405c159c72921429c4b6948e78
Shoma FUKUDA <fkshom@gmail.com>
2021-05-28 21:57:27 +0900

    wip

--- src/pyfixedwidths/__init__.py
+++ src/pyfixedwidths/__init__.py
@@ -1,25 +1,45 @@
 __version__ = '0.1.0'
 
 from pprint import pprint as pp
+from functools import reduce
+def _column_width(rows):
+    data_t = list(zip(*rows))
+    widths = list(map(lambda val: len(val), [max(row, key=len) for row in data_t]))
+    return widths
 
-def format(text):
-    def _column_width(rows):
-        data_t = list(zip(*rows))
-        widths = list(map(lambda val: len(val), [max(row, key=len) for row in data_t]))
-        return widths
+def format_dict(rows, padding=0):
+    def extract_headers(dict_of_array):
+        headers = [header for row in dict_of_array for header in row.keys()]
+        headers = list({value: "" for value in headers})
+        return headers
+    headers = extract_headers(rows)
 
-    lines = text.rstrip().split("\n")
-    rows = [line.split(',') for line in lines]
+    _rows = []
+    _rows.append(headers)
 
+    for row in rows:
+        values = list(map(lambda header: row.get(header, ''), headers))
+        _rows.append(values)
+    new_rows = format_array(_rows, padding=padding)
+    return "\n".join(new_rows) + "\n"
+    
+def format_array(rows, padding=0):
+    rows = [[str(val) for val in row] for row in rows]
     widths = _column_width(rows)
     pp(rows)
-    results = []
+    new_rows = []
     for row in rows:
         new_row = []
         for val, width in zip(row, widths):
             val = val.ljust(width)
             new_row.append(val)
-        results.append(','.join(new_row))
-    pp(results)
-    return "\n".join(results) + "\n"
+        new_rows.append((' ' * padding + ',' + ' ' * padding).join(new_row))
+    pp(new_rows)
+    return new_rows
 
+def format_text(text, padding=0):
+    lines = text.rstrip().split("\n")
+    rows = [line.split(',') for line in lines]
+
+    new_rows = format_array(rows, padding=padding)
+    return "\n".join(new_rows) + "\n"
--- tests/test_pyfixedwidths.py
+++ tests/test_pyfixedwidths.py
@@ -17,5 +17,46 @@ def test_run():
         "11,22,33,44\n"
     )
 
-    actual = pyfixedwidths.format(text)
+    actual = pyfixedwidths.format_text(text)
+    assert expected == actual
+
+def test_run2():
+    text = (
+        "1,2,3,4\n"
+        "11,22,33,44\n"
+    )
+
+    expected = (
+        "1  , 2  , 3  , 4 \n"
+        "11 , 22 , 33 , 44\n"
+    )
+
+    actual = pyfixedwidths.format_text(text, padding=1)
+    assert expected == actual
+
+def test_run3():
+    origin = [
+        dict(
+            firstname='Taro',
+            lastname='Tanaka',
+            age=20,
+            flag=True,
+            job='Student'
+        ),
+        dict(
+            firstname='Hanako',
+            lastname='Suzuki',
+            age=18,
+            flag=False,
+            hobby='Music'
+        )
+    ]
+
+    expected = (
+        "firstname , lastname , age , flag  , job     , hobby\n"
+        "Taro      , Tanaka   , 20  , True  , Student ,      \n"
+        "Hanako    , Suzuki   , 18  , False ,         , Music\n"
+    )
+
+    actual = pyfixedwidths.format_dict(origin, padding=1)
     assert expected == actual
