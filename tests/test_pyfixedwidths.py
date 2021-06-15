0a9b10f5f33b921f5d77e501937d1012099bd2c3
Shoma FUKUDA <fkshom@gmail.com>
2021-06-15 20:34:28 +0900

    wip

--- tests/test_pyfixedwidths.py
+++ tests/test_pyfixedwidths.py
@@ -1,18 +1,24 @@
-from pyfixedwidths import __version__
 import pyfixedwidths
-
 import pytest
 
-def test_version():
-    assert __version__ == '0.1.0'
 
 @pytest.fixture(scope='function', autouse=False)
 def sample_text1():
     text = (
         "1,2,3,4\n"
-        "11,22,33,44\n"
+        "11,,33,44\n"
     )
     yield text
+
+
+@pytest.fixture(scope='function', autouse=False)
+def sample_text2():
+    text = (
+        "1\t2\t3\t4\n"
+        "11\t\t33\t44\n"
+    )
+    yield text
+
 
 @pytest.fixture(scope='function', autouse=False)
 def sample_dict1():
@@ -22,20 +28,33 @@ def sample_dict1():
     ]
     yield listofdict
 
+
 @pytest.fixture(scope='function', autouse=False)
 def sample_array1():
     array = [
-        [1,None,3,4],
-        [11,22,None,44],
+        [1, None, 3, 4],
+        [11, 22, None, 44],
     ]
     yield array
+
+
+def test_usage(sample_dict1):
+    fw = pyfixedwidths.FixedWidthFormatter()
+    actual = fw.from_dict(sample_dict1).to_list()
+    expect = [
+        ["name      ", "age", "hobby", "job    "],
+        ['John Doe  ', "20 ", 'swim ', '       '],
+        ['John Smith', "100", '     ', 'teacher'],
+    ]
+    assert actual == expect
+
 
 def test_from(sample_text1, sample_dict1, sample_array1):
     fw = pyfixedwidths.FixedWidthFormatter()
     fw.from_text(sample_text1)
     assert fw._rows == [
         ["1", "2", "3", "4"],
-        ["11", "22", "33", "44"],
+        ["11", "", "33", "44"],
     ]
 
     fw.from_dict(sample_dict1)
@@ -45,20 +64,33 @@ def test_from(sample_text1, sample_dict1, sample_array1):
         ['John Smith', "100", '', 'teacher'],
     ]
 
+    fw.from_dict(sample_dict1, headers=["hobby", "job", "location", "name"])
+    assert fw._rows == [
+        ["hobby", "job", "location", "name"],
+        ['swim', '', '', 'John Doe'],
+        ['', 'teacher', '', 'John Smith'],
+    ]
+
     fw.from_list(sample_array1)
     assert fw._rows == [
-        ["1" , "None", "3", "4"],
+        ["1", "None", "3", "4"],
         ["11", "22", "None", "44"],
     ]
 
-def test_to(sample_dict1):
-    expected = [
-        ["1  ", " None ", " 3    ", " 4  "],
-        ["11 ", " 22   ", " None ", " 44 "],
+
+def test_from_text_with_sep(sample_text2):
+    fw = pyfixedwidths.FixedWidthFormatter()
+    fw.from_text(sample_text2, sep="\t")
+    assert fw._rows == [
+        ["1", "2", "3", "4"],
+        ["11", "", "33", "44"],
     ]
+
+
+def test_to(sample_dict1):
     fw = pyfixedwidths.FixedWidthFormatter()
     fw.from_dict(sample_dict1)
-    
+
     assert fw.to_list() == [
         ["name      ", "age", "hobby", "job    "],
         ['John Doe  ', "20 ", 'swim ', '       '],
@@ -74,6 +106,12 @@ def test_to(sample_dict1):
         "John Doe   , 20  , swim  ,        \n"
         "John Smith , 100 ,       , teacher"
     )
+    assert fw.to_text(padding=1, end='', sep="\t") == (
+        "name       \t age \t hobby \t job    \n"
+        "John Doe   \t 20  \t swim  \t        \n"
+        "John Smith \t 100 \t       \t teacher"
+    )
+
 
 def test_to_with_schema(sample_dict1):
     schema = [
@@ -101,35 +139,3 @@ def test_to_with_schema(sample_dict1):
         "  John Doe ,    20 , swim  ,   \n"
         "John Smith ,   100 ,       , teacher\n"
     )
-
-# def test_run4():
-#     schema = dict(
-#         age=dict(
-#             justification='rjust',
-#         )
-#     )
-#     origin = [
-#         dict(
-#             firstname='Taro',
-#             lastname='Tanaka',
-#             age=20,
-#             flag=True,
-#             job='Student'
-#         ),
-#         dict(
-#             firstname='Hanako',
-#             lastname='Suzuki',
-#             age=18,
-#             flag=False,
-#             hobby='Music'
-#         )
-#     ]
-
-#     expected = (
-#         "firstname , lastname , age , flag  , job     , hobby\n"
-#         "Taro      , Tanaka   ,  20 , True  , Student ,      \n"
-#         "Hanako    , Suzuki   ,  18 , False ,         , Music\n"
-#     )
-
-#     actual = pyfixedwidths.format_dict(origin, padding=1, schema=schema)
-#     assert expected == actual
